d88e4d2e13a20c9bbbc7ac2e97448ac86a1d47ff
joaskr <j.skrzypczak1998@gmail.com>
2024-06-10 23:00:39 +0200

    Added skeleton idle and move states

--- Assets/Scripts/EnemyState.cs
+++ Assets/Scripts/EnemyState.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class EnemyState
 {
     protected EnemyStateMachine stateMachine;
-    protected Enemy enemy;
+    protected Enemy enemyBase;
 
     protected bool triggerCalled;
     private string animBoolName;
     protected float stateTimer;
-    public EnemyState(Enemy _enemy, EnemyStateMachine _stateMachine, string _animBoolName)
+    public EnemyState(Enemy _enemyBase, EnemyStateMachine _stateMachine, string _animBoolName)
     {
-        this.enemy = _enemy;
+        this.enemyBase = _enemyBase;
         this.stateMachine = _stateMachine;
         this.animBoolName = _animBoolName;
     }
@@ -24,11 +24,11 @@ public class EnemyState
     public virtual void Enter()
     {
         triggerCalled = false;
-        enemy.animator.SetBool(animBoolName, true);
+        enemyBase.anim.SetBool(animBoolName, true);
     }
 
     public virtual void Exit()
     {
-        enemy.animator.SetBool(animBoolName, false);
+       enemyBase.anim.SetBool(animBoolName, false);
     }
 }
