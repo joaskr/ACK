83b8e804eebc350c5d08179dd5a023adb5aa3b4b
joaskr <j.skrzypczak1998@gmail.com>
2024-06-10 23:40:39 +0200

    Added sceleton player recognision

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Enemy : Entity
 {
+    [SerializeField] protected LayerMask whatIsPlayer;
     [Header("Move info")]
     public float moveSpeed;
     public float idleTime;
+
+    [Header("Attack info")]
+    public float attackDistance;
     public EnemyStateMachine stateMachine { get; private set; }
     protected override void Awake()
     {
@@ -18,5 +22,13 @@ public class Enemy : Entity
     {
         base.Update();
         stateMachine.currentState.Update();
+    }
+
+    public virtual RaycastHit2D IsPlayerDetected() => Physics2D.Raycast(wallCheck.position, Vector2.right * facingDir, 50, whatIsPlayer);
+    protected override void OnDrawGizmos()
+    {
+        base.OnDrawGizmos();
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x + attackDistance * facingDir, transform.position.y));
     }
 }
--- Assets/Scripts/Enemy_Skeleton.cs
+++ Assets/Scripts/Enemy_Skeleton.cs
@@ -7,12 +7,14 @@ public class Enemy_Skeleton : Enemy
     #region States
     public SkeletonIdleState idleState {  get; private set; }
     public SkeletonMoveState moveState { get; private set; }
+    public SkeletonBattleState battleState { get; private set; }
     #endregion
     protected override void Awake()
     {
         base.Awake();
         idleState = new SkeletonIdleState(this, stateMachine, "Idle", this);
         moveState = new SkeletonMoveState(this, stateMachine, "Move", this);
+        battleState = new SkeletonBattleState(this, stateMachine, "Move", this);
     }
 
     protected override void Start()
