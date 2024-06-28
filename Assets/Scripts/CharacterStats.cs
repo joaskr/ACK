091f4c5b383951714afdad08eea4e12e5eae89c9
joaskr <j.skrzypczak1998@gmail.com>
2024-06-28 17:30:34 +0200

    added additional stats

--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -1,14 +1,21 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
-    private int currentHealth;
-    public Stat strength;
-    public Stat damage;
+    [Header("Major stats")]
+    public Stat strength; //damage and crit power +1
+    public Stat agility; //evasion and crit chance +1%
+    public Stat intelligence; //magic resistance and magic damage +1
+    public Stat vitality; //health +3
+
+    [Header("Defensive stats")]
     public Stat maxHealth;
+    public Stat armor;
+    public Stat evasion;
+
+
+    private int currentHealth;
+    public Stat damage;
     // Start is called before the first frame update
     protected virtual void Start()
     {
@@ -17,19 +24,41 @@ public class CharacterStats : MonoBehaviour
 
     public virtual void DoDamage(CharacterStats _targetStats)
     {
+        if (TargetCanAvoidAttack(_targetStats))
+            return;
+
         int totalDamage = damage.GetValue() + strength.GetValue();
+
+        totalDamage = CheckTargetArmor(_targetStats, totalDamage);
         _targetStats.TakeDamage(totalDamage);
+    }
+
+    private int CheckTargetArmor(CharacterStats _targetStats, int totalDamage)
+    {
+        totalDamage -= _targetStats.armor.GetValue();
+        totalDamage = Mathf.Clamp(totalDamage, 0, int.MaxValue);
+        return totalDamage;
+    }
+
+    private bool TargetCanAvoidAttack(CharacterStats _targetStats)
+    {
+        int totalEvasion = _targetStats.evasion.GetValue() + _targetStats.agility.GetValue();
+        if (Random.Range(0, 100) < totalEvasion)
+        {
+            return true;
+        }
+        return false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    public virtual void TakeDamage (int _damage)
+    public virtual void TakeDamage(int _damage)
     {
         currentHealth -= _damage;
-        if(currentHealth < 0)
+        if (currentHealth < 0)
         {
             Die();
         }
