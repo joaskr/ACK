746a03875bdd27170c0089fa0afc3001c8ed8743
joaskr <j.skrzypczak1998@gmail.com>
2024-06-28 15:14:09 +0200

    Improved dash

--- Assets/Scripts/Player/PlayerDashState.cs
+++ Assets/Scripts/Player/PlayerDashState.cs
@@ -11,13 +11,14 @@ public class PlayerDashState : PlayerState
     public override void Enter()
     {
         base.Enter();
-        player.skill.clone.CreateClone(player.transform, Vector2.zero);
+        player.skill.clone.CreateCloneOnDashStart();
         stateTimer = player.dashDuration;
     }
 
     public override void Exit()
     {
         base.Exit();
+        player.skill.clone.CreateCloneOnDashOver();
         player.SetVelocity(0, rb.velocity.y);
     }
 
--- Assets/Scripts/Skills/CloneSkill.cs
+++ Assets/Scripts/Skills/CloneSkill.cs
@@ -9,9 +9,41 @@ public class CloneSkill : Skill
     [SerializeField] private float cloneDuration;
     [Space]
     [SerializeField] private bool canAttack;
+    [SerializeField] private bool createCloneOnDashStart;
+    [SerializeField] private bool createCloneOnDashOver;
+    [SerializeField] private bool canCreateCloneOnCounter;
     public void CreateClone(Transform _clonePosition, Vector3 _offset)
     {
         GameObject newClone = Instantiate(clonePrefab);
         newClone.GetComponent<CloneSkillController>().SetupClone(_clonePosition, cloneDuration, canAttack, _offset);
+    }
+
+    public void CreateCloneOnDashStart()
+    {
+        if(createCloneOnDashStart)
+        {
+            CreateClone(player.transform, Vector3.zero);
+        }
+    }
+
+    public void CreateCloneOnDashOver()
+    {
+        if (createCloneOnDashOver)
+        {
+            CreateClone(player.transform, Vector3.zero);
+        }
+    }
+
+    public void CreateCloneOnCounter(Transform _enemyTransform)
+    {
+        if (canCreateCloneOnCounter)
+        {
+            StartCoroutine(CreateCloneWithDelay(_enemyTransform, new Vector3(2 * player.facingDir, 0)));
+        }
+    }
+    private IEnumerator CreateCloneWithDelay(Transform _transform, Vector3 _offset)
+    {
+        yield return new WaitForSeconds(.4f);
+            CreateClone(_transform, _offset);
     }
 }
